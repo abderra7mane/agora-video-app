5d7860be4ed5685489f6e171214de117dfe24139
Abderrahmane Mokrani <m.abderra7mane@gmail.com>
2021-03-18 02:06:46 +0400

    Setup @angular/router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import AppRoutes from './app.routes';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(AppRoutes),
+    BrowserAnimationsModule,
     BrowserAnimationsModule
   ],
   providers: [],
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+
+const AppRoutes: Routes = [
+];
+
+export default AppRoutes;
